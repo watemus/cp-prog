a583a71355a075f46c0a6c53dbb03dacc47333d6
Semyon Tsekhnenko <watemus@gmail.com>
2020-03-24 19:13:34 +0300

    add simple stress

---
+++ simple_stress/gen.cpp
@@ -0,0 +1,12 @@
+//
+// Created by watemus on 24.03.2020.
+//
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+signed main(int argc, char **argv) {
+    mt19937 rnd(atoi(argv[1]));
+    return 0;
+}
