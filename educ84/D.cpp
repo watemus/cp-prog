f6701c3e87dfce205f813b2af6d6150305bd1d8f
Semyon Tsekhnenko <watemus@gmail.com>
2020-03-24 22:47:23 +0300

    add D educ 84 solution

--- educ84/D.cpp
+++ educ84/D.cpp
@@ -11,8 +11,50 @@ using ll = long long;
 using ld = long double;
 #define int ll
 
-void run() {
+constexpr ll INFL = 1'000'000'000'000'000'228;
+constexpr int INFI = 1'000'000'228;
 
+void run() {
+    int n;
+    cin >> n;
+    vector<int> p(n), c(n);
+    for (int i = 0; i < n; i++) {
+        cin >> p[i];
+        p[i]--;
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+    }
+    vector<int> usd(n);
+    int ans = INFL;
+    for (int i = 0; i < n; i++) {
+        if (!usd[i]) {
+            vector<int> cycle;
+            int j = i;
+            while (!usd[j]) {
+                cycle.push_back(j);
+                usd[j] = 1;
+                j = p[j];
+            }
+            for (int k = 1; k <= cycle.size(); k++) {
+                if (cycle.size() % k) continue;
+                for (int start = 0; start < k; start++) {
+                    bool checked = false;
+                    for (int cur = start + k; cur < cycle.size(); cur += k) {
+                        if (c[cycle[cur]] != c[cycle[cur - k]]) {
+                            checked = true;
+                            break;
+                        }
+                    }
+                    if (!checked) {
+                        ans = min(ans, k);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    cout << ans << '\n';
 }
 
 signed main() {
@@ -23,7 +65,7 @@ signed main() {
     cin.tie(nullptr);
 #endif
     int t = 1;
-//    cin >> t;
+    cin >> t;
     while (t--) {
         run();
     }
