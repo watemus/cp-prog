c829a37fb452b33545101d991ce8034f8f2d70cf
Semyon Tsekhnenko <watemus@gmail.com>
2020-03-27 22:10:51 +0300

    partically solved educ32 G

--- educ32/G.cpp
+++ educ32/G.cpp
@@ -21,18 +21,61 @@ using ld = long double;
 constexpr ll INFL = 1'000'000'000'000'000'228;
 constexpr int INFI = 1'000'000'228;
 
-constexpr int MAXN = 2e5;
+constexpr int MAXN = 2e5 + 228;
 constexpr int MAXK = 32;
 
-struct Vertex {
-    Vertex *go[2];
-};
+int trie[2][MAXN * MAXK];
+int cnt[MAXN * MAXK];
+int vs[MAXN * MAXK];
+int size_trie = 2;
 
-Vertex *root;
+int root = 1;
+
+void add_num(ll a, int v) {
+    int cur = root;
+    cnt[root]++;
+    for (int cbit = MAXK - 1; cbit >= 0; cbit--) {
+        int bit = bool((1LL << cbit) & a);
+        if (!trie[bit][cur]) {
+            trie[bit][cur] = size_trie++;
+        }
+        cur = trie[bit][cur];
+        cnt[cur]++;
+    }
+    vs[cur] = v;
+}
+
+vector<int> g[MAXN];
+
+void dfs(int v, int p, vector<int> &c, vector<int> &usd) {
+    c.push_back(v);
+    for (auto u : g[v]) {
+        if (u != p)
+            dfs(u, v, c, usd);
+    }
+}
 
 void run() {
     int n;
     cin >> n;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+        add_num(a[i], i);
+    }
+    int ans = 0;
+    for (int iter = 0; iter < 20; iter++) {
+        vector<int> usd(n);
+        set<pair<int, int>> add_edges;
+        for (int v = 0; v < n; v++) {
+            if (!usd[v]) {
+                vector<int> c;
+                dfs(v, v, c, usd);
+                if (c.size() == n) break;
+            }
+        }
+    }
+    cout << ans << '\n';
 }
 
 signed main() {
